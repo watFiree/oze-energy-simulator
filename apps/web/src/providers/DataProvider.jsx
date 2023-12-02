0879c60bccdc07fea7eeabf3b56f2c02a700c0c6
watFiree <hellocharlesp@gmail.com>
2023-12-02 10:23:27 +0100

    web: add simple websocket config

---
+++ apps/web/src/providers/DataProvider.jsx
@@ -0,0 +1,59 @@
+import {
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  useMemo,
+  createContext,
+} from "react";
+import { connectionStatus } from "./constants";
+
+export const DataContext = createContext();
+
+export const DataProvider = ({ children }) => {
+  const connection = useRef(null);
+  const [status, setStatus] = useState(connectionStatus.DISCONNECTED);
+  const [error, setError] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    connection.current = new WebSocket(`ws://localhost:6969`);
+    connection.current.onopen = () => {
+      setStatus(connectionStatus.CONNECTED);
+    };
+    connection.current.onclose = () => {
+      setStatus(connectionStatus.DISCONNECTED);
+    };
+    connection.current.onerror = (err) => {
+      setError(err);
+    };
+    connection.current.onmessage = (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    };
+
+    return () => {
+      connection.current.close();
+    };
+  }, []);
+
+  const sendMessage = useCallback(
+    (msg) => {
+      connection.current.send(msg);
+    },
+    [connection]
+  );
+
+  const contextValue = useMemo(
+    () => ({
+      status,
+      error,
+      messages,
+      sendMessage,
+    }),
+    [status, error, messages, sendMessage]
+  );
+
+  return (
+    <DataContext.Provider value={contextValue}>{children}</DataContext.Provider>
+  );
+};
---
+++ apps/web/src/providers/constants.ts
@@ -0,0 +1,4 @@
+export const connectionStatus = {
+  CONNECTED: "connected",
+  DISCONNECTED: "disconnected",
+};
